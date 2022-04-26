e52ffaa36620dea6ba1408faabbe95d38d720abc
roy6307 <a01024070849@gmail.com>
2022-04-27 01:28:49 +0900

    On branch master  Changes to be committed: 	modified:   Goose Goose Goose.vcxproj 	modified:   Goose Goose Goose.vcxproj.filters 	modified:   dllmain.cpp 	new file:   struct.hpp
    More bugs and more bugs.

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,6 +7,7 @@ Why? Wrong PlayerController reference?
 
 #include "utils.hpp"
 #include "offsets.hpp"
+#include "struct.hpp"
 
 #include "MinHook/include/MinHook.h"
 #include "kiero/kiero.h"
@@ -29,86 +30,6 @@ using namespace std;
 typedef HRESULT(WINAPI* PRESENT)(IDXGISwapChain*, UINT, UINT);
 typedef LRESULT(WINAPI* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 typedef void* (*tFlip)(void* PlayerController, int someval1, int someval2, void* PhotonData); // void Flip(PlayerController this, int NBPPNBKBMNF, int JMAHGLDEHHB, PhotonMessageInfo BJMKOJJELHA){}
-
-// from imgui_demo.cpp
-struct ExampleAppLog
-{
-	ImGuiTextBuffer     Buf;
-	ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
-	bool                AutoScroll;  // Keep scrolling if already at the bottom.
-
-	ExampleAppLog()
-	{
-		AutoScroll = true;
-		Clear();
-	}
-
-	void Clear()
-	{
-		Buf.clear();
-		LineOffsets.clear();
-		LineOffsets.push_back(0);
-	}
-
-	void AddLog(const char* fmt, ...) IM_FMTARGS(2)
-	{
-		int old_size = Buf.size();
-		va_list args;
-		va_start(args, fmt);
-		Buf.appendfv(fmt, args);
-		va_end(args);
-		for (int new_size = Buf.size(); old_size < new_size; old_size++)
-			if (Buf[old_size] == '\n')
-				LineOffsets.push_back(old_size + 1);
-	}
-
-	void Draw(const char* title, bool* p_open = NULL)
-	{
-		if (!ImGui::Begin(title, p_open))
-		{
-			ImGui::End();
-			return;
-		}
-
-		// Options menu
-		ImGui::Checkbox("Auto-scroll", &AutoScroll);
-
-		// Main window
-		ImGui::SameLine();
-		bool clear = ImGui::Button("Clear");
-
-		ImGui::Separator();
-		ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-
-		if (clear)
-			Clear();
-
-		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-		const char* buf = Buf.begin();
-		const char* buf_end = Buf.end();
-
-		ImGuiListClipper clipper;
-		clipper.Begin(LineOffsets.Size);
-		while (clipper.Step())
-		{
-			for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
-			{
-				const char* line_start = buf + LineOffsets[line_no];
-				const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-				ImGui::TextUnformatted(line_start, line_end);
-			}
-		}
-		clipper.End();
-
-		ImGui::PopStyleVar();
-
-		if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
-			ImGui::SetScrollHereY(1.0f);
-
-		ImGui::EndChild();
-		ImGui::End();
-	}
-};
 
 ExampleAppLog appLog;
 
@@ -135,19 +56,11 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 		return false;
 	}
 
-	if (canRender) {
+
+	if (canRender)
 		if (ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam)) return true;
 
-		else return false;
-	}
-	else return CallWindowProc(oWndproc, hWnd, uMsg, wParam, lParam);
-
-	/*if (canRender)
-		if (ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam)) return true;
-
-	if (!canRender)
-		if (CallWindowProc(oWndproc, hWnd, uMsg, wParam, lParam)) return true;
-		else return false;*/
+		return CallWindowProc(oWndproc, hWnd, uMsg, wParam, lParam);
 
 }
 
@@ -158,9 +71,8 @@ tFlip hkFlip(void* PlayerController, int someval1, int someval2, void* PhotonDat
 		if ((DWORD_PTR)PlayerController != *ListIterator) cnt++;
 	}
 
-	//if (PlayerControllerList.size() == 0) { PlayerControllerList.push_back((DWORD_PTR)PlayerController); cout << "Add new: " << (DWORD_PTR)PlayerController << endl; }
-	if (PlayerControllerList.size() == 0) { PlayerControllerList.push_back((DWORD_PTR)PlayerController); appLog.AddLog("[info] Add new PlayerController: %lld\n",(DWORD_PTR)PlayerController); }
-	if (PlayerControllerList.size() != 0 && PlayerControllerList.size() == cnt) { PlayerControllerList.push_back((DWORD_PTR)PlayerController); appLog.AddLog("[info] Add new PlayerController: %lld\n",(DWORD_PTR)PlayerController); }
+	if (PlayerControllerList.size() == 0) { PlayerControllerList.push_back((DWORD_PTR)PlayerController); appLog.AddLog("[info] Add new PlayerController: %12llX\n",(DWORD_PTR)PlayerController); }
+	if (PlayerControllerList.size() != 0 && PlayerControllerList.size() == cnt) { PlayerControllerList.push_back((DWORD_PTR)PlayerController); appLog.AddLog("[info] Add new PlayerController: %12llX\n",(DWORD_PTR)PlayerController); }
 
 	MH_DisableHook(oFlip);
 	tFlip ret_val = (tFlip)oFlip(PlayerController, someval1, someval2, PhotonData);
@@ -220,7 +132,7 @@ HRESULT WINAPI hkPre(IDXGISwapChain* pSC, UINT SyncInterval, UINT Flags)
 			ImGui::Begin("Player list");
 			if (ImGui::Button("Clear")) {
 				PlayerControllerList.clear();
-				CurrentIdx = -1;
+				CurrentIdx = -1;       ///// bbbbbbbbbbbaaaaaaaadddddddddd iiiiiiddddddddeeeeeeeeeeaaaaaaaaa
 			}
 
 			if (ImGui::BeginListBox("PlayerControllerDataListBox", ImVec2(-FLT_MIN, 5 * ImGui::GetTextLineHeightWithSpacing()))) {
@@ -228,23 +140,17 @@ HRESULT WINAPI hkPre(IDXGISwapChain* pSC, UINT SyncInterval, UINT Flags)
 				for (ListIterator = PlayerControllerList.begin(); ListIterator != PlayerControllerList.end(); ListIterator++) {
 
 					int cnt = 0;
-					wchar_t nickname[17] = L"";
-					char WCTMBnickname[64] = "";
 					const bool is_selected = (CurrentIdx == cnt);
+					playerInfo player;
+					player.update(*ListIterator);
 
-					DWORD_PTR pNickname = *ListIterator + GooseGooseDuck::PlayerController::nickname;
-					int nicknameLength = *(int*)(*(DWORD_PTR*)(pNickname)+0x10);
-
-					memcpy(nickname, (DWORD_PTR*)(*(DWORD_PTR*)(pNickname)+0x14), sizeof(wchar_t) * nicknameLength);
-					int len = WideCharToMultiByte(CP_UTF8, 0, nickname, -1, NULL, 0, NULL, NULL);
-					WideCharToMultiByte(CP_UTF8, 0, nickname, -1, WCTMBnickname, len, NULL, NULL);
-
-					if (ImGui::Selectable(WCTMBnickname, is_selected))
+					if(ImGui::Selectable(player.nickname,is_selected))
 						CurrentIdx = cnt;
-
+						
 					if (is_selected) {
 						ImGui::SetItemDefaultFocus();
-						appLog.AddLog(u8"[info] %lld\nNickname pointer: %p\nNickname: %s\n", *ListIterator, (DWORD_PTR*)(*(DWORD_PTR*)(pNickname)+0x14), WCTMBnickname);
+						appLog.AddLog(u8"[player info]\nPlayerController: %012llX\nNickname: %s\nisRoleSet: %s\nRole: %d\n",
+													*ListIterator, player.nickname, player.isPlayerRoleSet ? "True" : "False", player.playerRole);
 						CurrentIdx = -1;
 					}
 					cnt++;
@@ -265,7 +171,6 @@ void MainFunc(HMODULE hModule) {
 
 	if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
 	{
-		//if (AllocConsole()) freopen("CONOUT$", "wb", stdout);
 
 		// define KIERO_USE_MINHOOK must be 1
 		// the index of the required function can be found in the METHODSTABLE.txt
---
+++ struct.hpp
@@ -0,0 +1,139 @@
+#include "imgui/imgui.h"
+
+// from imgui_demo.cpp
+struct ExampleAppLog
+{
+	ImGuiTextBuffer     Buf;
+	ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
+	bool                AutoScroll;  // Keep scrolling if already at the bottom.
+
+	ExampleAppLog()
+	{
+		AutoScroll = true;
+		Clear();
+	}
+
+	void Clear()
+	{
+		Buf.clear();
+		LineOffsets.clear();
+		LineOffsets.push_back(0);
+	}
+
+	void AddLog(const char* fmt, ...) IM_FMTARGS(2)
+	{
+		int old_size = Buf.size();
+		va_list args;
+		va_start(args, fmt);
+		Buf.appendfv(fmt, args);
+		va_end(args);
+		for (int new_size = Buf.size(); old_size < new_size; old_size++)
+			if (Buf[old_size] == '\n')
+				LineOffsets.push_back(old_size + 1);
+	}
+
+	void Draw(const char* title, bool* p_open = NULL)
+	{
+		if (!ImGui::Begin(title, p_open))
+		{
+			ImGui::End();
+			return;
+		}
+
+		// Options menu
+		ImGui::Checkbox("Auto-scroll", &AutoScroll);
+
+		// Main window
+		ImGui::SameLine();
+		bool clear = ImGui::Button("Clear");
+
+		ImGui::Separator();
+		ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+		if (clear)
+			Clear();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+		const char* buf = Buf.begin();
+		const char* buf_end = Buf.end();
+
+		ImGuiListClipper clipper;
+		clipper.Begin(LineOffsets.Size);
+		while (clipper.Step())
+		{
+			for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+			{
+				const char* line_start = buf + LineOffsets[line_no];
+				const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+				ImGui::TextUnformatted(line_start, line_end);
+			}
+		}
+		clipper.End();
+
+		ImGui::PopStyleVar();
+
+		if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+			ImGui::SetScrollHereY(1.0f);
+
+		ImGui::EndChild();
+		ImGui::End();
+	}
+};
+
+struct playerInfo {
+	bool isSilenced = false;
+	bool isInfected = false;
+	int playerRole = 0;
+	bool isPlayerRoleSet = false;
+	bool inVent = false;
+	bool hasBomb = false;
+	bool isGhost = false;
+	bool isLocal = false;
+	int invisibilityDistance = 0;
+	bool isSpectator = false;
+	bool isRemoteSpectating = false;
+	char nickname[17] = "";
+
+	void reset() {
+		isSilenced = false;
+		isInfected = false;
+		playerRole = 0;
+		isPlayerRoleSet = false;
+		inVent = false;
+		hasBomb = false;
+		isGhost = false;
+		isLocal = false;
+		invisibilityDistance = 0;
+		isSpectator = false;
+		isRemoteSpectating = false;
+	}
+
+	void update(DWORD_PTR PlayerController) {
+
+		wchar_t tmpNick[17] = L"";
+
+		memcpy(tmpNick, (DWORD_PTR*)(*(DWORD_PTR*)(PlayerController + GooseGooseDuck::PlayerController::nickname) + 0x14), sizeof(wchar_t) * *(int*)(*(DWORD_PTR*)(PlayerController + GooseGooseDuck::PlayerController::nickname) + 0x10));
+		int len = WideCharToMultiByte(CP_UTF8, 0, tmpNick, -1, NULL, 0, NULL, NULL);
+		WideCharToMultiByte(CP_UTF8, 0, tmpNick, -1, nickname, len, NULL, NULL);
+
+#define GET_BOOL_VALUE(X) *(bool*)(PlayerController+X)
+#define GET_INT_VALUE(X) *(int*)(PlayerController+X)
+
+		isPlayerRoleSet = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isPlayerRoleSet);
+
+		if (isPlayerRoleSet) {
+			isSilenced = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isSilenced);
+			isInfected = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isInfected);
+			playerRole = *(int*)(*(DWORD_PTR*)(PlayerController + GooseGooseDuck::PlayerController::playerRole) + 0x10);
+			inVent = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::inVent);
+			hasBomb = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::hasBomb);
+			isGhost = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isGhost);
+			isLocal = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isLocal);
+			invisibilityDistance = GET_INT_VALUE(GooseGooseDuck::PlayerController::invisibilityDistance);
+			isSpectator = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isSpectator);
+			isRemoteSpectating = GET_BOOL_VALUE(GooseGooseDuck::PlayerController::isRemoteSpectating);
+		}
+#undef GET_BOOL_VALUE
+#undef GET_INT_VALUE
+	}
+};
